3d8aa3f3574eb10d7bfb97deb26c91af47a94e2c
Joel Grus <joelgrus@gmail.com>
2021-01-17 18:53:26 -0800

    corrupt

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+const randomChar = () => {
+    const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    const pos = Math.floor(Math.random() * charSet.length)
+    return charSet[pos]
+}
+
+const corrupt = (s) => {
+    const pos = Math.floor(Math.random() * s.length)
+    return s.slice(0, pos) + randomChar() + s.slice(pos+1)
+}
+
 const generateNumber = () => {
     while (true) {
-        const number = (10 * Math.random()) ** (20 * Math.random()) * (-0.5 + Math.random())
+        var number = ((10 * Math.random()) ** (20 * Math.random()) * (-0.5 + Math.random())).toString()
 
-        if (number.toString().indexOf('e') == -1) {
+        if (number.indexOf('e') == -1) {
+            if (Math.random() < 0.1) {
+                number = corrupt(number)
+            }
+
             document.getElementById("number").innerText = number    
             return
         }
